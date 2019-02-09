997c80f3c34594579f8b4511ab2b48e9dcc071f1
Dave Cohen <git@dave-cohen.com>
2019-02-09 08:01:08 -0800

    gocopy: added trim (-trim) and verbose (-v) flags
    Useful when copying passwords, i.e. `pwgen -s 22 | gocopy -v -trim`

--- cmd/gocopy/gocopy.go
+++ cmd/gocopy/gocopy.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/atotto/clipboard"
@@ -11,15 +13,27 @@ import (
 
 func main() {
 	timeout := flag.Duration("t", 0, "Erase clipboard after timeout.  Durations are specified like \"20s\" or \"2h45m\".  0 (default) means never erase.")
+	verbose := flag.Bool("v", false, "Verbose output.")
+	trim := flag.Bool("trim", false, "Trim whitespace.")
 	flag.Parse()
 
-	out, err := ioutil.ReadAll(os.Stdin)
+	in, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
 		panic(err)
 	}
+	var out string
+	if *trim {
+		out = strings.TrimSpace(string(in))
+	} else {
+		out = string(in)
+	}
 
-	if err := clipboard.WriteAll(string(out)); err != nil {
+	if err := clipboard.WriteAll(out); err != nil {
 		panic(err)
+	}
+	if *verbose {
+		fmt.Println(out, []byte(out))
+		fmt.Printf("wrote %d bytes to clipboard\n", len(out))
 	}
 
 	if timeout != nil && *timeout > 0 {
@@ -28,7 +42,7 @@ func main() {
 		if err != nil {
 			os.Exit(1)
 		}
-		if text == string(out) {
+		if text == out {
 			err = clipboard.WriteAll("")
 		}
 	}
